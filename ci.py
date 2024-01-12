a2c9dad3af685e50c58bdf8a9024a9947bafe014
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2024-01-12 10:20:11 -0500

    Update ci.py

--- ci.py
+++ ci.py
@@ -2,4 +2,4 @@ import wandb
 
 print(f'The version of wandb is: {wandb.__version__}')
 
-assert wandb.__version__ == '0.16.1', f'Expected version 0.16.1, but got {wandb.__version__}'
+assert wandb.__version__ == '0.16.2', f'Expected version 0.16.2, but got {wandb.__version__}'
